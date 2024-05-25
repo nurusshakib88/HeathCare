f34be23b0444d17a4563a5a40057006b3d46a1f8
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2024-05-25 22:27:35 +0600

    Add proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001', // Change this to your backend server URL
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
