fec25b017c4352adcb719cb64048075d6ae1c0b6
Nurus Shakib Rhythm <84782668+nurusshakib88@users.noreply.github.com>
2024-06-02 22:53:46 +0600

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
+import Chatbot from "./pages/Chatbot";
 
 import { ProtectedRoute } from "./protected/ProtectedRoutes";
 import UserProfile from "./pages/UserProfile";
@@ -23,7 +24,6 @@ import BloodInventory from "./pages/BloodInventory";
 import BloodDonorList from "./pages/BloodDonorList";
 import BloodRequest from "./pages/BloodRequest";
 import Ambulance from "./pages/Ambulance";
-import ChatBot from "./components/ChatBot";
 import { useEffect } from "react";
 import Contact from "./pages/Contact";
 
@@ -51,7 +51,7 @@ const App = () => {
       {!isDashboard && !isBloodBank && <Navbar />}
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/chat" element={<ChatBot />} />
+        <Route path="/chat" element={<Chatbot />} />
         <Route path="/" element={<Home />} />
         <Route path="/ambulance" element={<Ambulance></Ambulance>} />
         <Route path="/make-an-appointment" element={<MakeAnAppointment />} />
